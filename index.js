c95c235915b842dd363a9875372304365bf1d4ec
Roberto Luna <robertoluna508@gmail.com>
2021-11-17 10:19:20 -0600

    Archivos iniciales

---
+++ index.js
@@ -0,0 +1,11 @@
+//Modulos requeridos
+const express = require('express');
+const bodyparser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+
+//express
+const app = express();
+console.log('hola 123');
+
+app.listen(3000);
