a5f0595f4391b1dafd40dcc84e26a03936cdd310
Roberto Luna <robertoluna508@gmail.com>
2021-11-26 15:11:04 -0600

    correcion de errores

---
+++ uberservicio/src/app/model/pedido.ts
@@ -0,0 +1,17 @@
+export class Pedido {
+    idPedido: number = 0;
+    fechaInicio:Date=new Date();
+    fechaFin:Date=new Date();
+    horaInicio: Date=new Date();
+    horaFin: Date=new Date();
+    total:number = 0;
+
+    constructor() {
+        this.idPedido=this.idPedido;
+        this.fechaInicio=this.fechaInicio;
+        this.fechaFin=this.fechaFin;
+        this.horaInicio=this.horaInicio;
+        this.horaFin=this.horaFin;
+        this.total=this.total
+    }
+}
