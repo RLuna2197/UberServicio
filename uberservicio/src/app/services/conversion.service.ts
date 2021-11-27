05e332de834c5ed0fafaafb00ec3a8f8f8d35164
Roberto Luna <robertoluna508@gmail.com>
2021-11-27 17:00:32 -0600

    Realizar pedido completado

--- uberservicio/src/app/services/conversion.service.ts
+++ uberservicio/src/app/services/conversion.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { Bitcoin } from '../model/bitcoin';
+import { Euro } from '../model/euro';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConversionService {
   url:string = environment.baseUrl;
-  
   constructor(private http: HttpClient) { }
 
   getValorEuro(){
@@ -18,6 +19,17 @@ export class ConversionService {
       "llave": "Bearer eyJhbGciOiJIUzI1NiJ9.cm9iZXJMdW5hbHVuYTEyMw.BBD_ybOCN5btQEEzkN76HJ2u1bRTbD9zTr3lhYZr408"
       })
       };
-    return this.http.get<any[]>(`${this.url}/Comentarios/5`,httpOptions);
+    return this.http.get<Euro>(`${this.url}/api/euro`,httpOptions);
+  }
+
+  getValorBitcoin(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+      'Access-Control-Allow-Origin':'*',
+      'Content-Type': 'application/json',
+      "llave": "Bearer eyJhbGciOiJIUzI1NiJ9.cm9iZXJMdW5hbHVuYTEyMw.BBD_ybOCN5btQEEzkN76HJ2u1bRTbD9zTr3lhYZr408"
+      })
+      };
+    return this.http.get<Bitcoin>(`${this.url}/api/bitcoin`,httpOptions);
   }
 }
--- uberservicio/src/app/ver-servicio/ver-servicio.component.ts
+++ uberservicio/src/app/ver-servicio/ver-servicio.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Bitcoin } from '../model/bitcoin';
 import { Comentario } from '../model/comentario';
+import { Euro } from '../model/euro';
+import { Pedido } from '../model/pedido';
 import { Servicio } from '../model/servicio';
 import { ComentarioService } from '../services/comentario.service';
+import { ConversionService } from '../services/conversion.service';
+import { PedidoService } from '../services/pedido.service';
 import { PersonaService } from '../services/persona.service';
 import { ServicioService } from '../services/servicio.service';
 
@@ -16,13 +21,25 @@ export class VerServicioComponent implements OnInit {
   servicios: Servicio[] = [];
   comentarios: any[] = [];
   personas: any[] =[];
+  euro: Euro = new Euro();
+  bitcoin: Bitcoin = new Bitcoin();
   idPersona: string= "" ;
   inputComentario: string="";
   califi: number=0;
+  inputPrecio: number=0;
   comentarioNuevo: Comentario = new Comentario();
-  
+  pedidoNuevo: Pedido = new Pedido();
+  conversionEuro: number=0;
+  conversionBitcoin: number=0;
+  opcionSeleccionado: number=0;
+  total: number=0;
+  simbolo: string = "$";
+  inputFechaInicio: Date = new Date();
+  inputFechaFin: Date = new Date();
+  inputHoraInicio: Date = new Date();
+  inputHoraFin: Date = new Date();
 
-  constructor(private fb:FormBuilder, private dataApi: ServicioService , private datApiComen: ComentarioService, private dataApiPerson: PersonaService) {
+  constructor(private fb:FormBuilder, private dataApi: ServicioService , private datApiComen: ComentarioService, private dataApiPerson: PersonaService, private DataConversion: ConversionService, private DataPedio: PedidoService) {
     this.formularioComentario=this.fb.group({
       comentario:['',Validators.required],
       calificacion:['',Validators.required]    
@@ -32,13 +49,16 @@ export class VerServicioComponent implements OnInit {
   ngOnInit( ): void {
 
     localStorage.setItem('id', '7');
-    
+    this.getEuro();
+    this.getBitcoin();
     this.getServicios();
     this.getComentarios();
+    
     //Obteniendo el usuario logeado
     this.idPersona = localStorage.getItem('id') as string;
     this.getPersonaByid(this.idPersona);
-
+    
+   
     this.formularioComentario=this.fb.group({
       comentario:['',Validators.required],
       calificacion:['',Validators.required]    
@@ -50,6 +70,7 @@ export class VerServicioComponent implements OnInit {
 
     this.dataApi.getServiciosByid().subscribe((response) => {
       this.servicios = response;
+      this.inputPrecio = Number(this.servicios[0].precio);
     },
       (error) => { console.error(error); }
     );
@@ -80,6 +101,32 @@ export class VerServicioComponent implements OnInit {
   }
 
 
+  //Obtener Valor Euro
+  private getEuro(){
+    this.DataConversion.getValorEuro().subscribe((response) => {
+      this.euro.conversion_rate = response.conversion_rate;
+      this.conversionEuro = this.euro.conversion_rate;
+    },
+      (error) => { console.error(error); }
+    );
+   
+    //this.DataConversion.getValorEuro().subscribe((euro) => console.log(euro.conversion_rate));
+    
+  }
+
+  //Obtener Valor Bitcoin
+  private getBitcoin(){
+    this.DataConversion.getValorBitcoin().subscribe((response) => {
+      this.bitcoin.rate = response.rate;
+      this.conversionBitcoin = this.bitcoin.rate;
+    },
+      (error) => { console.error(error); }
+    );
+   
+    //this.DataConversion.getValorEuro().subscribe((euro) => console.log(euro.conversion_rate));
+    
+  }
+
   //Comentario
   calificacion(cali: number){
     this.califi = cali; 
@@ -101,6 +148,42 @@ export class VerServicioComponent implements OnInit {
 
     this.getComentarios();
     this.getServicios();
+  }
+
+
+  agregarPedido(){
+    this.pedidoNuevo.fechaInicio = this.inputFechaInicio;
+    this.pedidoNuevo.fechaFin = this.inputFechaFin;
+    this.pedidoNuevo.horaInicio = this.inputHoraInicio;
+    this.pedidoNuevo.horaFin = this.inputHoraFin;
+    this.pedidoNuevo.total = this.total;
+    this.pedidoNuevo.idPedido = 5;
+
+    this.DataPedio.savePedido(this.pedidoNuevo).subscribe(res => {
+      console.log(res);
+
+    }, err => {
+      console.log(err);
+    })
+    
+  }
+
+  //Asignar cambio
+  monedaCambio(){
+      if(this.opcionSeleccionado == 1){
+        this.simbolo = "$";
+        this.total = this.inputPrecio;
+      }
+      if(this.opcionSeleccionado == 3){
+        this.simbolo="₿";
+        this.getBitcoin();
+        this.total = this.inputPrecio * this.conversionBitcoin;
+      }
+      if(this.opcionSeleccionado == 2){
+        this.simbolo="€";
+        this.getEuro();
+        this.total = this.inputPrecio * this.conversionEuro;
+      }
   }
   
 }
